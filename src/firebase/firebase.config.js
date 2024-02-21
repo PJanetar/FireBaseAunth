c6b7228be3955bf10ababce3dd0c972492964718
Perla Janet Argumedo <pedrozap920@gmail.com>
2024-02-20 23:26:29 -0700

    Primer commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA30jjgjajKOb_dc-0UbbTCclRkJANDdEI",
+  authDomain: "coffeandcode-b448d.firebaseapp.com",
+  projectId: "coffeandcode-b448d",
+  storageBucket: "coffeandcode-b448d.appspot.com",
+  messagingSenderId: "1081378904702",
+  appId: "1:1081378904702:web:c11deb4955de919e3fd62f"
+};
+
+const app = initializeApp(firebaseConfig);
